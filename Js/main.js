90f3c7b34ffdddda0e1e877b6ebccb8f10278ee1
LexLuthor666 <128583749+LexLuthor666@users.noreply.github.com>
2023-03-22 12:58:30 +0300

    Delete Js directory

--- Js/main.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".bike__slider, .slider__items").slick({
-    arrows: false,
-    dots: true,
-    fade: true, // меняет стиль слайдера
-    autoplay: true, // автосмена картинок слайда
-    autoplaySpeed: 2000, //скорость смены картинок(ms)
-  });
-});
